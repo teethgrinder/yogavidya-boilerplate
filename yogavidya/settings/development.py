35cdf6ed9fd36f905a83c0f68425dbf426f3def3
Burak AKIN <burak.akin@gmail.com>
2016-10-02 22:48:45 +0300

    started pssql database

--- yogavidya/settings/development.py
+++ yogavidya/settings/development.py
@@ -6,6 +6,18 @@ from .base import *
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = '7ig$oax5u^kscl2&luld&htts@4a=sq29zu6($+oix@v7=k7)q'
+ 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
+        'HOST': '',
+        'PORT': '',
+    }
+}
 
 STATIC_URL = '/static/'
  
